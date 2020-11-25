fde02361d021905f6403c63a8786a91d900c36e8
zenyth1 <anr9698@gmail.com>
2020-11-25 13:16:39 +1000

    Renamed get_current to get_last

--- asx_fetch.py
+++ asx_fetch.py
@@ -7,11 +7,11 @@ def get_header_path(ticker: str) -> str:
     """Returns the request url for the 'header' information for a given ticker."""
     return asx_base_url + "/" + ticker + "/header"
 
-def get_current_price(ticker: str) -> str:
+def get_last_price(ticker: str) -> str:
     response = urllib.request.urlopen(get_header_path(ticker)).read().decode()
     data = json.loads(response)
     return data['data']['priceLast']
 
 if __name__ == "__main__":
-    print(get_current_price("VDHG"))
-    print(get_current_price("MSB"))
+    print(get_last_price("VDHG"))
+    print(get_last_price("MSB"))
