1a5723ea54da5687f4d06186433186fb61a455d6
zenyth1 <anr9698@gmail.com>
2020-11-26 16:03:18 +1000

    Added get_key_stats function

--- asx_fetch.py
+++ asx_fetch.py
@@ -7,15 +7,20 @@ def get_header_path(ticker: str) -> str:
     """Returns the request url for the 'header' information for a given ticker."""
     return asx_base_url + "/" + ticker + "/header"
 
-def get_last_price(ticker: str) -> str:
+def get_key_stats_path(ticker: str) -> str:
+    """Returns the request url for the 'key statistics' information for a given ticker."""
+    return asx_base_url + "/" + ticker + "/key-statistics"
+
+def get_last_price(ticker: str) -> float:
+    """Returns the last price for a given ticker."""
     response = urllib.request.urlopen(get_header_path(ticker)).read().decode()
     data = json.loads(response)
-    return data['data']['priceLast']
+    return float(data['data']['priceLast'])
 
 def get_key_stats(ticker: str) -> object:
-    placeholder = {}
-    return placeholder
+    response = urllib.request.urlopen(get_key_stats_path(ticker)).read().decode()
+    data = json.loads(response)
+    return data
 
 if __name__ == "__main__":
-    print(get_last_price("VDHG"))
-    print(get_last_price("MSB"))
+    print(get_key_stats("VDHG"))
